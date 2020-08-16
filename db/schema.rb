5c072002071297ffcb81f1456310fea3eaf391eb
山根寛信 <yamanehironobu@yamanehironobunoMacBook-Air.local>
2020-08-16 12:41:10 +0900

    user-model-create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200703062501) do
+ActiveRecord::Schema.define(version: 20200816033558) do
 
   create_table "categories", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -27,13 +27,12 @@ ActiveRecord::Schema.define(version: 20200703062501) do
     t.string "image"
   end
 
-  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
